f5a5cd6996cdbfd21c8e1d4c4cd49766d2f633c7
posullivan <paul_os2002@yahoo.co.uk>
2015-01-26 15:53:56 +0000

    Initial commit

---
+++ rest-spring-jersey/rest-spring-jersey/src/com/posullivan/example/rest/Launch.java
@@ -0,0 +1,12 @@
+package com.posullivan.example.rest;
+
+public class Launch {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Launch !!!");
+	}
+
+}
